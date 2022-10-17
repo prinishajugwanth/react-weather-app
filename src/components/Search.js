4a441c499b01055df522c6759c90d64d542e871f
prinishajugwanth <prinisham@gmail.com>
2022-10-17 16:11:46 +0200

    forecast
    bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,115 +1,120 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Spinner from "react-bootstrap/Spinner";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
+import Modal from 'react-bootstrap/Modal';
 
-import TemperatureDetails from "./TemperatureDetails";
+import TemperatureDetails from './TemperatureDetails';
 
 export default function Search() {
-  let [located, setLocated] = useState(false);
-  let [latitude, setLatitude] = useState(0);
-  let [longitude, setLongitude] = useState(0);
-  let [cityname, setCityname] = useState("");
-  let [currentWeatherConditions, setCurrentWeatherConditions] = useState();
-  let [loadingGeo, setLoadingGeo] = useState(false);
-  let [loadingSearch, setLoadingSearch] = useState(false);
+    let [located, setLocated] = useState(false);
+    let [latitude, setLatitude] = useState(0);
+    let [longitude, setLongitude] = useState(0);
+    let [cityname, setCityname] = useState('');
+    let [currentWeatherConditions, setCurrentWeatherConditions] = useState();
+    let [loadingGeo, setLoadingGeo] = useState(false);
+    let [loadingSearch, setLoadingSearch] = useState(false);
+    let [errorMessage, setErrorMessage] = useState('');
 
-  const apiId = "45a279e4eb49922fc1c93e07d331e80a";
+    const [showModalError, setShowModalError] = useState(false);
+    const handleClose = () => setShowModalError(false);
+    const handleShow = () => setShowModalError(true);
 
-  function geoLocateUser(event) {
-    if (navigator.geolocation) {
-      setLoadingGeo(true);
-      setLoadingSearch(false);
-      setLocated(false);
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-        resolveCityNameFromGeoLocation(position.coords);
-      });
+    const apiId = '45a279e4eb49922fc1c93e07d331e80a';
+
+    function geoLocateUser(event) {
+        if (navigator.geolocation) {
+            setLoadingGeo(true);
+            setLoadingSearch(false);
+            setLocated(false);
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+                resolveCityNameFromGeoLocation(position.coords);
+            });
+        }
     }
-  }
 
-  function resolveCityNameFromGeoLocation(coords) {
-    let getCityNameUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${apiId}&units=metric`;
-    axios.get(getCityNameUrl).then(function (data) {
-      setCityname(data.data.name);
-      setCurrentWeatherConditions(data.data);
-      setLocated(true);
-      setLoadingGeo(false);
-      setLoadingSearch(false);
-    });
-  }
+    function resolveCityNameFromGeoLocation(coords) {
+        let getCityNameUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${apiId}&units=metric`;
+        axios.get(getCityNameUrl).then(function (data) {
+            setCityname(data.data.name);
+            setCurrentWeatherConditions(data.data);
+            setLocated(true);
+            setLoadingGeo(false);
+            setLoadingSearch(false);
+        });
+    }
 
-  function updateCity(event) {
-    setLocated(false);
-    setCityname(event.target.value);
-  }
+    function updateCity(event) {
+        setLocated(false);
+        setCityname(event.target.value);
+    }
+    function updateCityEnter(event) {
+        if (event.key === 'Enter') {
+            searchUserCity();
+        }
+    }
 
-  function searchUserCity(event) {
-    setLoadingGeo(false);
-    setLoadingSearch(true);
-    setLocated(false);
-    resolveGeolocationFromCityName(cityname);
-  }
+    function searchUserCity(event) {
+        setLoadingGeo(false);
+        setLoadingSearch(true);
+        setLocated(false);
+        resolveGeolocationFromCityName();
+    }
 
-  function resolveGeolocationFromCityName() {
-    let getCoordinatesUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiId}&units=metric`;
-    axios.get(getCoordinatesUrl).then(function (data) {
-      setCityname(data.data.name);
-      setLatitude(data.data.coord.lat);
-      setLongitude(data.data.coord.lon);
-      setCurrentWeatherConditions(data.data);
-      setLocated(true);
-      setLoadingGeo(false);
-      setLoadingSearch(false);
-    });
-  }
+    function resolveGeolocationFromCityName() {
+        let getCoordinatesUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiId}&units=metric`;
+        axios
+            .get(getCoordinatesUrl)
+            .then(function (data) {
+                setCityname(data.data.name);
+                setLatitude(data.data.coord.lat);
+                setLongitude(data.data.coord.lon);
+                setCurrentWeatherConditions(data.data);
+                setLocated(true);
+                setLoadingGeo(false);
+                setLoadingSearch(false);
+            })
+            .catch(function (error) {
+                console.log(error);
+                setErrorMessage(error.response.data.message);
+                setLoadingGeo(false);
+                setLoadingSearch(false);
+                setLocated(false);
+                handleShow();
+            });
+    }
 
-  return (
-    <>
-      <InputGroup className="px-2">
-        <Form.Control
-          placeholder="Enter City"
-          aria-label="city"
-          onChange={updateCity}
-        />
-        <Button variant="outline-secondary" onClick={geoLocateUser}>
-          {!loadingGeo && <i className="bi bi-geo"></i>}
-          {loadingGeo && (
-            <Spinner
-              as="span"
-              animation="border"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-            />
-          )}
-          <span className="visually-hidden">Loading...</span>
-        </Button>
-        <Button variant="outline-secondary" onClick={searchUserCity}>
-          {!loadingSearch && <i className="bi bi-search"></i>}
-          {loadingSearch && (
-            <Spinner
-              as="span"
-              animation="border"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-            />
-          )}
-        </Button>
-      </InputGroup>
-      {located && (
-        <TemperatureDetails
-          latitude={latitude}
-          longitude={longitude}
-          name={cityname}
-          currentConditions={currentWeatherConditions}
-        />
-      )}
-    </>
-  );
+    return (
+        <>
+            <InputGroup className='px-2'>
+                <Form.Control placeholder='Enter City' aria-label='city' onChange={updateCity} onKeyPress={updateCityEnter} />
+                <Button variant='outline-secondary' onClick={geoLocateUser}>
+                    {!loadingGeo && <i className='bi bi-geo'></i>}
+                    {loadingGeo && <Spinner as='span' animation='border' size='sm' role='status' aria-hidden='true' />}
+                    <span className='visually-hidden'>Loading...</span>
+                </Button>
+                <Button variant='outline-secondary' onClick={searchUserCity}>
+                    {!loadingSearch && <i className='bi bi-search'></i>}
+                    {loadingSearch && <Spinner as='span' animation='border' size='sm' role='status' aria-hidden='true' />}
+                </Button>
+            </InputGroup>
+            {located && <TemperatureDetails latitude={latitude} longitude={longitude} name={cityname} currentConditions={currentWeatherConditions} />}
+            <Modal show={showModalError} onHide={handleClose} backdrop='static' keyboard={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Weather Application</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className='text-capitalize'>{errorMessage}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-import "./index.css";
+import './index.css';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import Search from "./components/Search";
+import Search from './components/Search';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Stack from "react-bootstrap/Stack";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Stack from 'react-bootstrap/Stack';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <div className="WeatherApp">
-      <Stack gap={3}>
-        <header className="Header">Weather Application</header>
-        <Search className="Search" />
-        <footer className="Footer">
-          <small>
-            This page was designed by Prinisha Jugwanth and is an{" "}
-            <a href="https://github.com/prinishajugwanth/react-weather-app">
-              open-source code
-            </a>
-          </small>
-        </footer>
-      </Stack>
-    </div>
-  </React.StrictMode>
+    <React.StrictMode>
+        <div className='WeatherApp mt-0'>
+            <Stack gap={3}>
+                <header className='Header'>Weather Application</header>
+                <Search className='Search' />
+                <footer className='Footer'>
+                    <h6 className='mb-0 fw-normal'>
+                        This page was designed by Prinisha Jugwanth and is an <a href='https://github.com/prinishajugwanth/react-weather-app'>open-source code</a>
+                    </h6>
+                </footer>
+            </Stack>
+        </div>
+    </React.StrictMode>
 );
